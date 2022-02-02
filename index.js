a4e0f6395197836d2367c0d764d0d32ae28a46c1
Arnold Grosberg <arnold.grosberg@khk.ee>
2022-02-02 16:31:46 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -10,6 +12,24 @@ const key = '10ed3bdf99d81facf362c6aa87f1ef4e';
 let city = 'Tartu'
 
 app.get('/', function(req,res){
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+	.then((responce) => {
+		return responce.json()
+	})
+	.then((data) => {
+		let description = data.weather[0].description
+		let city = data.name
+		let temp = Math.round(parseFloat(data.main.temp)-273.15)
+		res.render('default', {
+			description: description,
+			city: city,
+			temp: temp
+		})
+	})
+})
+
+app.post('/', function(req,res){
+	let city = req.body.cityname
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
 	.then((responce) => {
 		return responce.json()
